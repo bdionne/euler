a702b57e3f03c74e492959675e11fb092f80da1b
Bob Dionne <dionne@dionne-associates.com>
2008-11-12 08:26:21 -0500

    added comment

--- prob1.erl
+++ prob1.erl
@@ -1,7 +1,7 @@
 -module(prob1).
 -export([run/0]).
 %% If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-%% The sum of these multiples is 23.
+%% The sum of these multiples is 23, foo.
 %% Find the sum of all the multiples of 3 or 5 below 1000.
 run() ->
     run(3, 1000, 0).
